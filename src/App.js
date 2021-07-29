b9cbee3f747d3848149daaf00d056590304ff65c
mli-sela <mlisela.mngqinya@gmail.com>
2021-07-29 19:03:15 +0200

    Second phase

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import './App.css';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Weather from './components/weather';
+import Loader from './components/loader';
 
 function App() {
-  
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [forecastData, setForecastData] = useState([]);
-  
-  // used to load the function when app loads | reloads
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setForecastData(result)
-        console.log(result);
-      });
-    }
-    fetchData();
-  }, [lat, long]);
+	const [lat, setLat] = useState([]);
+	const [long, setLong] = useState([]);
+	const [forecastData, setForecastData] = useState([]);
 
-  return (
-    <div className="App">
-      {(typeof forecastData.main !== 'undefined') ? (
-        <Weather weatherData={forecastData}/>
-      ): (
-        <div>No data</div>
-      )}
-    </div>
-  );
+	// used to load the function when app loads | reloads
+	useEffect(() => {
+		const fetchData = async () => {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				setLat(position.coords.latitude);
+				setLong(position.coords.longitude);
+			});
+
+			await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+				.then(res => res.json())
+				.then(result => {
+					setForecastData(result)
+					console.log(result);
+				});
+		}
+		fetchData();
+	}, [lat, long]);
+
+	return (
+		<div className="App">
+			{(typeof forecastData.main !== 'undefined') ? (
+				<Weather weatherData={forecastData} />
+			) : (
+				<Loader />
+			)}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loader from 'react-loader-spinner';
+
+const LoaderSpinner = () => (
+    <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+    />
+);
+
+export default LoaderSpinner
